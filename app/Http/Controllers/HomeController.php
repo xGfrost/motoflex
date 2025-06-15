03b82ef80b38e36f54b99817debc1c65a83cc596
Aswin-art <aswinarung2@gmail.com>
2025-06-15 20:58:44 +0700

    fix: home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,13 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        $workshops = Workshops::query()->withAvg('ratings', 'rating')->get();
+        $workshops = Workshops::query()
+        ->withAvg('ratings', 'rating')
+        ->orderByDesc('ratings_avg_rating')
+        ->orderByDesc('created_at')
+        ->limit(5)
+        ->get();
+
         $data = [
             'user' => $user,
             'workshops' => $workshops
